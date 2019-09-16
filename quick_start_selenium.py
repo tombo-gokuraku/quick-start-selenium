ee2fc793d3de0582c5e616c507bb622e5a0717db
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-09-16 20:03:38 +0900

    crawl python.org

--- quick_start_selenium.py
+++ quick_start_selenium.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+
+# from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urljoin
+import time
+from pprint import pprint
 
 #  headless mode を使う:  <15-09-19, yourname> #
 
 URL = "http://www.python.org"
-SEARCH_WORD = "GCP"
+SEARCH_WORD = "AWS"
 
 contents_list = []
 
@@ -23,37 +27,65 @@ search_box.send_keys(SEARCH_WORD)
 search_box.send_keys(Keys.RETURN)
 
 #  Nextボタンが無くなるまでループする:  <15-09-19, yourname> #
-
-# ページのロードを待機する
-try:
-    search_results = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(
-            (By.CSS_SELECTOR, "ul.list-recent-events li p")
+while True:
+    # ページのロードを待機する
+    try:
+        search_results = WebDriverWait(driver, 5).until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, "ul.list-recent-events li")
+            )
         )
-    )
-except Exception as e:
-    print(e)
-    raise
+    except TimeoutException:
+        raise
 
-# 検索結果を取得する
-search_result = driver.find_element_by_css_selector("ul.list-recent-events li")
-# print(type(search_result), search_result)
-# title = search_result.find_element_by_css_selector("h3 a").text()
-title = search_result.find_element_by_css_selector("h3 a").text
-print(title)
-url = urljoin(
-    URL, search_result.find_element_by_css_selector("h3 a").get_attribute("href")
-)
-print(url)
-description_text = ""
-descriptions = search_result.find_elements_by_css_selector("p")
-for description in descriptions:
-    description_text += description.text + "\n"
+    # search_results = driver.find_elements_by_css_selector("ul.list-recent-events li")
+    # 検索結果を取得する
+    for search_result in search_results:
+        # search_result = driver.find_element_by_css_selector("ul.list-recent-events li")
+        # print(type(search_result), search_result)
+        # title = search_result.find_element_by_css_selector("h3 a").text()
+        title = search_result.find_element_by_css_selector("h3 a").text
+        # print(title)
+        url = urljoin(
+            URL,
+            search_result.find_element_by_css_selector("h3 a").get_attribute("href"),
+        )
+        # print(url)
+        description_text = ""
+        descriptions = search_result.find_elements_by_css_selector("p")
+        for description in descriptions:
+            description_text += description.text + "\n"
 
-print(description_text)
+        # print(description_text)
 
-#  検索結果をlistに格納する:  <15-09-19, yourname> #
+        item = {"url": url, "title": title, "description": description_text}
+        # print(item)
 
-# Nextボタンを押す
+        #  検索結果をlistに格納する:  <15-09-19, yourname> #
+        contents_list.append(item)
+
+    # Nextボタンを押す
+    try:
+        next_elem = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    # "//div[a[contains(@href,'page')]][a[contains(text(),'Next')]]",
+                    "//div/a[contains(@href,'page')][contains(text(),'Next')]",
+                )
+            )
+        )
+        print("go to next")
+        # next_elem.click()
+        driver.get(next_elem.get_attribute("href"))
+        time.sleep(1)
+    except TimeoutException:
+        print("end of search result")
+        break
+
 
 # 結果を出力する
+pprint(contents_list[:5])
+print(len(contents_list))
+
+driver.close()
