5a8d80dff459d94bca52f95147fd944a3dab0cad
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-09-16 20:03:53 +0900

    testing wait

---
+++ wait_test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+driver = webdriver.Firefox()
+driver.get("https://www.python.org")
+
+try:
+    search_results = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input#id-search-field"))
+    )
+    print("do something")
+except TimeoutException:
+    raise
