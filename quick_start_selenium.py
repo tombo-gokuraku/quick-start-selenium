2be000270fc0b99200ec45699878c43c6708ed07
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-09-20 11:48:33 +0900

    add save json

--- quick_start_selenium.py
+++ quick_start_selenium.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 
-# from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,8 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urljoin
 import time
 from pprint import pprint
+import json
 
-#  headless mode を使う:  <15-09-19, yourname> #
 
 URL = "http://www.python.org"
 SEARCH_WORD = "AWS"
@@ -38,45 +37,31 @@ while True:
     except TimeoutException:
         raise
 
-    # search_results = driver.find_elements_by_css_selector("ul.list-recent-events li")
     # 検索結果を取得する
     for search_result in search_results:
-        # search_result = driver.find_element_by_css_selector("ul.list-recent-events li")
-        # print(type(search_result), search_result)
-        # title = search_result.find_element_by_css_selector("h3 a").text()
         title = search_result.find_element_by_css_selector("h3 a").text
-        # print(title)
         url = urljoin(
             URL,
             search_result.find_element_by_css_selector("h3 a").get_attribute("href"),
         )
-        # print(url)
         description_text = ""
         descriptions = search_result.find_elements_by_css_selector("p")
         for description in descriptions:
             description_text += description.text + "\n"
 
-        # print(description_text)
-
         item = {"url": url, "title": title, "description": description_text}
-        # print(item)
 
         #  検索結果をlistに格納する:  <15-09-19, yourname> #
         contents_list.append(item)
 
-    # Nextボタンを押す
+    # 次のページへ移動する
     try:
         next_elem = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    # "//div[a[contains(@href,'page')]][a[contains(text(),'Next')]]",
-                    "//div/a[contains(@href,'page')][contains(text(),'Next')]",
-                )
+                (By.XPATH, "//div/a[contains(@href,'page')][contains(text(),'Next')]")
             )
         )
         print("go to next")
-        # next_elem.click()
         driver.get(next_elem.get_attribute("href"))
         time.sleep(1)
     except TimeoutException:
@@ -87,5 +72,8 @@ while True:
 # 結果を出力する
 pprint(contents_list[:5])
 print(len(contents_list))
+
+with open("./search_reslut.json", "w") as f:
+    json.dump(contents_list, f, indent=2)
 
 driver.close()
